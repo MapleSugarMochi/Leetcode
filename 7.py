aaffb2efc16299f8ca66574183eba0a3fe974f13
Xuzhou Dong <dongxuzhou0032@gmail.com>
2026-01-20 23:57:27 +0800

    01.20.2026

---
+++ 7.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        negative = (x < 0)
+        x = abs(x)
+        ans = 0
+        while x > 0:
+            digit = x % 10
+            x = int(x / 10)
+            ans = ans + digit
+            ans = ans * 10
+        ans = ans / 10
+        if ans < -2 ** 31 or 2 ** 31 - 1 < ans:
+            return 0
+        if negative:
+            return int(0 - ans)
+        else:
+            return int(ans)
