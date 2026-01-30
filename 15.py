21bf8b8ba8b3ca8de3e16af1cf0df5a5fe0b6ebb
Xuzhou Dong <dongxuzhou0032@gmail.com>
2026-01-31 00:42:46 +0800

    31/01/2026

---
+++ 15.py
@@ -0,0 +1,23 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        for a in range(len(nums)):
+            if a > 0 and nums[a] == nums[a - 1]: continue
+            if nums[a] > 0: break
+            left = a + 1
+            right = len(nums) - 1
+            while left < right:
+                if nums[left] + nums[right] + nums[a] == 0:
+                    ans.append([nums[a], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    while left < right and nums[left] == nums[left - 1]:
+                        left += 1
+                    while left < right and nums[right] == nums[right + 1]:
+                        right -= 1
+                elif nums[left] + nums[right] > 0 - nums[a]:
+                    right -= 1
+                else:
+                    left += 1
+        return ans
