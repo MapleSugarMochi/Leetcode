b197f790bcec289f0e31b6f68bc6f36e9c8e3b59
Xuzhou Dong <dongxuzhou0032@gmail.com>
2026-02-01 07:39:48 +0800

    01/02/2026

---
+++ 3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        longest = 1
+        lst = list(s)
+        if len(lst) == 0:
+            return 0
+        elif len(lst) == 1:
+            return 1
+        else:
+            for i in range(len(lst) - 1):
+                length = 0
+                record = {}
+                record[lst[i]] = True
+                for j in range(i + 1, len(lst)):
+                    if record.get(lst[j]) == None:
+                        record[lst[j]] = True
+                        length += 1
+                        longest = max(longest, length + 1)
+                    else:
+                        record.clear()
+                        break
+        return longest
