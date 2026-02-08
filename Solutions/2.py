6502a830d2ea086a6a35ffa2d6647a33f65d6051
Xuzhou Dong <dongxuzhou0032@gmail.com>
2026-02-08 21:54:06 +0800

    08/02/2026

--- Solutions/2.py
+++ Solutions/2.py
@@ -1,18 +1,44 @@
+"""
+LeetCode 2. Add Two Numbers
+Difficulty: Medium
+Category: Linked_List
+
+Approach:
+- Simulate column addition
+- Time: O(n)
+- Space: O(n)
+"""
+
 class Solution:
-    def addTwoNumbers(l1, l2):
+    def addTwoNumbers(self, listnode1: Optional[ListNode], listnode2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        def to_list(head):
+            arr = []
+            while head:
+                arr.append(head.val)
+                head = head.next
+            return arr
+
+        def build_list(arr):
+            dummy = ListNode()
+            cur = dummy
+            for x in arr:
+                cur.next = ListNode(x)
+                cur = cur.next
+            return dummy.next
+
         ans = []
-        if len(l1) > len(l2): l2 = [0] * (len(l1) - len(l2)) + l2
-        else: l1 = [0] * (len(l2) - len(l1)) + l1
+        l1 = to_list(listnode1)
+        l2 = to_list(listnode2)
+        if len(l1) > len(l2): l2 = l2 + [0] * (len(l1) - len(l2))
+        else: l1 = l1 + [0] * (len(l2) - len(l1))
         for i in range(len(l1)):
-            ans.append(l1[-1-i] + l2[-1-i])
+            ans.append(l1[i] + l2[i])
         for i in range(len(ans) - 1):
             if ans[i] >= 10:
                 ans[i] -= 10
                 ans[i+1] += 1
-        print(ans)
         if ans[-1] >= 10:
             ans[-1] -= 10
             ans.append(1)
-        return ans
-    
-    print(addTwoNumbers([2,4,3], [5,6,4]))
+        return build_list(ans)
